46763ed5ec968cb84ff8034a85d71d025749d84a
Craig Mulligan <hobochildster@gmail.com>
2019-09-19 22:22:06 +0100

    Handle subcommand edge cases
    Fixes #22
    Adds similarity check to for subcommand typos.
    Cleans object of self refs so we don't infinitely recurse
    Orders subcommands alphabetically
    
    exit 1 if command not found
    
    switch to enquirer for prompt
    
    fixup interactive tests
    
    add interactive tests

---
+++ examples/circular-reference.js
@@ -0,0 +1,10 @@
+const fire = require('../')
+
+const calculator = {
+  half: number => +number / 2,
+  double: number => +number * 2,
+}
+
+calculator['self'] = calculator
+
+fire(calculator)
--- index.test.js
+++ index.test.js
@@ -1,8 +1,8 @@
 const fire = require('./')
 const util = require('util')
 const fs = require('fs')
-const exec = util.promisify(require('child_process').exec)
-const pty = require('node-pty')
+const execCb = require('child_process').exec
+const exec = util.promisify(execCb)
 
 describe('js-fire', () => {
   describe('function', () => {
@@ -24,6 +24,15 @@ describe('js-fire', () => {
     test('helpText', async () => {
       const { stdout } = await exec('node examples/arrow-function.js --help')
       expect(stdout).toMatchSnapshot()
+    })
+  })
+
+  describe('circular reference', async () => {
+    test('should compile', async () => {
+      const { stdout } = await exec(
+        'node examples/circular-reference.js half --number 20',
+      )
+      expect(stdout.trim()).toEqual('10')
     })
   })
 
@@ -62,22 +71,45 @@ describe('js-fire', () => {
   })
 
   describe('interactive', () => {
+    const keys = {
+      up: '\x1B\x5B\x41',
+      down: '\x1B\x5B\x42',
+      enter: '\x0D',
+      space: '\x20',
+    }
+
     test('object with subcommand', done => {
-      const ps = pty.spawn('node', ['examples/object.js', 'half', '-i'], {
-        env: process.env,
+      const ps = execCb('node examples/object.js half -i', (err, stdout) => {
+        expect(stdout.trim()).toContain('10')
+        done()
       })
 
-      let count = 0
-      ps.on('data', data => {
-        if (count == 7) {
-          expect(data.toString().trim()).toEqual('10')
-          done()
-        }
+      ps.stdin.write(`20${keys.enter}`)
+    })
 
-        count++
+    test('object from root', done => {
+      const ps = execCb('node examples/object.js misc -i', (err, stdout) => {
+        console.log(stdout)
+        expect(stdout.trim()).toContain('greetings hobo!')
+        done()
       })
 
-      ps.write('20\r')
+      setTimeout(() => ps.stdin.write(`info\r\n`), 100)
+      setTimeout(() => ps.stdin.write(`greeting\r\n`), 200)
+      setTimeout(() => ps.stdin.write(`hobo\r\n`), 400)
+    })
+
+    test('object from root', done => {
+      const ps = execCb('node examples/object.js -i', (err, stdout) => {
+        console.log(stdout)
+        expect(stdout.trim()).toContain('greetings hobo!')
+        done()
+      })
+
+      setTimeout(() => ps.stdin.write(`misc\r\n`), 100)
+      setTimeout(() => ps.stdin.write(`info\r\n`), 200)
+      setTimeout(() => ps.stdin.write(`greeting\r\n`), 400)
+      setTimeout(() => ps.stdin.write(`hobo\r\n`), 600)
     })
   })
 
@@ -92,10 +124,25 @@ describe('js-fire', () => {
       expect(stdout.trim()).toMatchSnapshot()
     })
 
-    test('helpText with subcommand', async () => {
+    test('helpText with subcommand without help flag', async () => {
       const { stdout } = await exec('js-fire fs -h')
       // Command usage should ref js-fire
       expect(stdout.trim()).toMatchSnapshot()
+    })
+
+    test('helpText with subcommand with help flag', async () => {
+      const { stdout } = await exec('js-fire fs -h')
+      expect(stdout.trim()).toMatchSnapshot()
+    })
+
+    test('helpText with incorrect subcommand name', async () => {
+      // this should return a suggested command in the output
+      try {
+        const { stdout } = await exec('js-fire fs appendF')
+        // this should return an exit code 1
+      } catch (err) {
+        expect(err.stdout.trim()).toMatchSnapshot()
+      }
     })
 
     test('bin with named args', async () => {
@@ -143,6 +190,29 @@ describe('js-fire', () => {
 
     test('helpText with subcommand', async () => {
       const { stdout } = await exec('node examples/object.js double --help')
+      expect(stdout).toMatchSnapshot()
+    })
+
+    test('command not found', async () => {
+      // test case for:
+      // https://github.com/hobochild/js-fire/issues/22
+      const { stdout } = await exec(
+        'node examples/object.js misc; echo "exit code $?"',
+      )
+      expect(stdout).toMatchSnapshot()
+    })
+
+    test('deeply nested', async () => {
+      const { stdout } = await exec(
+        'node examples/object.js misc info greeting --name hobochild',
+      )
+      expect(stdout.trim()).toEqual('greetings hobochild!')
+    })
+
+    test('deeply nested with typo', async () => {
+      const { stdout } = await exec(
+        'node examples/object.js misc info greeting --name hobochild',
+      )
       expect(stdout).toMatchSnapshot()
     })
   })
